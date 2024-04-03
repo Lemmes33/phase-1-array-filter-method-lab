1d271b9c34777cdb1c076da4a2284abfdead71e6
lemmes33 <vinlemuel021@gmail.com>
2024-04-03 16:03:24 +0300

    array lab array filter

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    
+    const lowercaseName = name.toLowerCase();
+  
+    return drivers.filter(driver => driver.toLowerCase().includes(lowercaseName));
+  }
+  
+  function fuzzyMatch(drivers, partialName) {
+    const lowercasePartialName = partialName.toLowerCase();
+  
+    return drivers.filter(driver => driver.toLowerCase().startsWith(lowercasePartialName));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name); 
+  }
+  
+
